6fba4ba2d482a936b6110723155c7392c0979cfc
shubhamkumarsingh <www.shubhamsingh6666@gmail.com>
2022-09-23 10:08:59 +0530

    first commit

---
+++ sumofdiagonal/src/App.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        {
+            int a[][] = new int[3][3];
+            int i, j, sum = 0;
+            System.out.println("Enter the array element \n");
+            for (i = 0; i < 3; i++) {
+                for (j = 0; j < 3; j++) {
+                    a[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println("Entered array element's are \n");
+            for (i = 0; i < 3; i++) {
+                for (j = 0; j < 3; j++) {
+                    System.out.println(+a[i][j] + "\t");
+                }
+            }
+            System.out.println("Sum of diagonal element \n");
+            for (i = 0; i < 3; i++) {
+                for (j = 0; j < 3; j++) {
+                    if (a[i] == a[j]) {
+                        sum = sum + a[i][j];
+                        System.out.println(+sum);
+                    }
+                }
+            }
+
+        }
+    }
+}
